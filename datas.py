6bf1c7ff6e06289cfc5c874947fc8e2dc5af70d5
ArunmathavanK <133943485+ArunmathavanK@users.noreply.github.com>
2024-08-23 12:25:35 +0530

    Add files via upload

---
+++ datas.py
@@ -0,0 +1,104 @@
+data=[
+    {
+        'name':'Cristiano Ronaldo',
+        'category':'Footballer',
+        'followers':636,
+        'country':'Portugal'
+    },
+    {
+        'name':'Sachin Tendulkar',
+        'category':'Cricketer',
+        'followers':50,
+        'country':'India'
+    },
+    {
+        'name':'Lional Messi',
+        'category':'Footballer',
+        'followers':504,
+        'country':'Argentina'
+    },
+    {
+        'name':'Real Madrid CF',
+        'category':'Football club',
+        'followers':166,
+        'country':'Spain'
+    },
+    {
+        'name':'Lebran James',
+        'category':'Basket ball Player',
+        'followers':160,
+        'country':'USA'
+    },
+    {
+        'name':'Virat Kohli',
+        'category':'Cricketer',
+        'followers':270,
+        'country':'India'
+    },
+    {
+        'name':'Neymar JR',
+        'category':'Freestyller',
+        'followers':224,
+        'country':'Brazil'
+    },
+    {
+        'name':'The Rock',
+        'category':'Wrestler',
+        'followers':396,
+        'country':'US'
+    },
+    {
+        'name':'Kylie jenner',
+        'category':'Media personality',
+        'followers':398,
+        'country':'US'
+    },
+    {
+        'name':'Kylin Mbappe',
+        'category':'Footballer',
+        'followers':121,
+        'country':'France'
+    },
+    {
+        'name':'FC Barcelona',
+        'category':'Football club',
+        'followers':129,
+        'country':'Spain'
+    },
+    {
+        'name':'Shraddha Kapoor',
+        'category':'Actress',
+        'followers':92,
+        'country':'India'
+    },
+    {
+        'name':'Justin Bieber',
+        'category':'Musician',
+        'followers':293,
+        'country':'Canada'
+    },
+    {
+        'name':'Mr Beast',
+        'category':'Youtuber',
+        'followers':230,
+        'country':'US'
+    },
+    {
+        'name':'Priyanka Chopra',
+        'category':'Actress',
+        'followers':91,
+        'country':'India'
+    },
+    {
+        'name':'David Beckham',
+        'category':'Former Footballer',
+        'followers':88,
+        'country':'England'
+    },
+    {
+        'name':'Selena Gomez',
+        'category':'Musician',
+        'followers':425,
+        'country':'US'
+    }
+]
